cea3f49d22e7ac80e1c61ccae5f84074a6fbbd40
Zoestra <79121105+Zoestra@users.noreply.github.com>
2025-11-02 09:25:39 -0800

    adds week 4 practice, aboutme and contact

---
+++ week4/ad320learningpracticejsx-Zoestra/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
+import Projects from "./Projects";
+
+function App() {
+	return (
+		<>
+			<h1>Profile</h1>
+			<AboutMe />
+			<Contact />
+			<Projects />
+		</>
+	);
+}
+
+export default App;
